4ab1f740f5ae8121edfe3e47d3dde78db30b370a
RarchikCreation <chimmichurri.bad@gmail.com>
2025-10-19 12:15:58 +0300

    config.py start.py

--- settings/config.py
+++ settings/config.py
@@ -5,3 +5,4 @@ dotenv_path = os.path.join("data", ".env")
 load_dotenv(dotenv_path)
 
 TOKEN=os.getenv("TOKEN")
+ADMINS = [int(x.strip()) for x in os.getenv("ADMINS", "").split(",") if x.strip().isdigit()]
